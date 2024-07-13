50d77172f1b343436f1984542cc422d98dcf2bb1
agusbudiawan080810 <164469436+agusbudiawan080810@users.noreply.github.com>
2024-07-13 15:45:28 +0000

    Pending changes exported from your codespace

---
+++ .bash_history
@@ -0,0 +1,21 @@
+xdotool getmouselocation
+xdotool getmouselocation
+xdotool getmouselocation
+xdotool getmouselocation
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+clear
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
+./check_codesanbox.sh 
---
+++ check_codesanbox.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+COORDINATE_CLOSE_TAB="252 46"
+COORDINATE_OPEN_TAB="141 46"
+
+start_container=1
+end_container=5
+
+while [ $start_container -le $end_container ]
+do
+
+    xdotool mousemove $COORDINATE_OPEN_TAB click 3
+    sleep 1
+
+    xdotool key --repeat 7 Down key Right
+    sleep 1
+
+    xdotool key --repeat $start_container Down key Return
+    sleep 5
+
+    echo "container $((start_container + 1)) is open now!"
+
+    xdotool mousemove $COORDINATE_CLOSE_TAB click 1
+    sleep 2
+
+    # 😎 repeat next step... 
+    ((start_container++))
+done
